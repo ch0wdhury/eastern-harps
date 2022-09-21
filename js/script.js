81ca4f3d10d06494f2bddb585722cdb12e8cbd42
Ashiq Ali Chowdhury <43188066+ch0wdhury@users.noreply.github.com>
2022-09-21 20:32:52 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+
+    $('.customer-logos').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 3
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 2
+            }
+        }]
+    });
